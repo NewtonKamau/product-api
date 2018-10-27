6e3f6d08b15c24b7954df50b5aeec50f049c4438
Newton <kamaunewton78@gmail.com>
2018-10-27 14:02:16 +0300

    added the get, add products

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
-const axios = require('axios')
+const mysql = require('mysql');
+
+
+// create the mysql connection
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'product_api'
+});
+
+//test the connection
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting to database: ' + err.stack);
+    return;
+  }
+
+  console.log('Database connected! ' + connection.threadId);
+});
 
 const upload = multer(); // parse multi-part/form-data
 const app = express();
@@ -17,16 +36,33 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // parsing application/x-www-form-urlencodedapp.
 app.get('/api/getproducts', (req, res) => {
   //Fetch all products from dB 
+  connection.query('SELECT * FROM product', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+    res.status(200).json(results);
+  });
+  connection.end();
  
 });
 
 app.post('/api/addProduct', upload.array(), (req, res) => {
+  const product = {
+    "product_name": req.body.name,
+    "product_category": req.body.category
+  }
   //add products to db
+  connection.query('INSERT INTO product  SET ? ', product, function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+   res.status(200).json(results);
+  });
+  connection.end();
   
    
 });
 app.post('/api/deleteProduct', upload.array(), (req, res) => {
  //delete a particular product using the id
+
 });
 
 app.post('/api/editproduct', upload.array(), (req, res) => {
@@ -34,7 +70,16 @@ app.post('/api/editproduct', upload.array(), (req, res) => {
 
 });
 app.post('/api/addCategory', upload.array(), (req, res) => {
-
+ const category = {
+   "category_name": req.body.category
+ }
+ //add products to db
+ connection.query('INSERT INTO category  SET ? ', category, function (error, results, fields) {
+   if (error) throw error;
+   console.log('The solution is: ', results);
+   res.status(200).json(results);
+ });
+ connection.end();
 });
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
